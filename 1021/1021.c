7d16939b473b69aafd1a18c8b9bd02494b4520d6
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-10-14 13:18:27 +0900

    1014

--- 1021/1021.c
+++ 1021/1021.c
@@ -2,8 +2,7 @@
 
 int main()
 {
-    int inum1, inum2;
-
-    scanf("%d-%d", &inum1, &inum2);
-    printf("%06d%07d", inum1, inum2);
+    char data[51] = "";
+    scanf("%s", data);
+    printf("%s", data);
 }
